e7066e288ca7f7f07919d3e73239e66db1ba2d1a
Sunaa Risu <sunaa@sunaarisu.de>
2025-01-18 14:14:02 +0100

    updated functionality

--- Stitch/src/Renderer/shader.h
+++ Stitch/src/Renderer/shader.h
@@ -3,20 +3,36 @@
 
 #include <GL/glew.h>
 #include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource {
+  std::string VertexSource;
+  std::string FragmentSource;
+};
 
 class Shader {
+private:
+  std::string m_SrcFilePath;
+  unsigned int m_ProgramID;
+  std::unordered_map<std::string, int> m_UniformLocationCache;
+
 public:
   Shader(const std::string &filePath);
-
-  void Bind();
-  void Update();
-
-  inline int GetUniformLocation(const char* name) { return glGetUniformLocation(m_program, name); }
-
   ~Shader();
 
+  void Bind() const;
+  void Unbind() const;
+  void Update() const;
+
+  // Set uniforms
+  void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);  
+
 private:
-  unsigned int m_program;
+  ShaderProgramSource ParseShader(const std::string& filePath);
+  unsigned int CompileShader(unsigned int type, const std::string& source);
+  unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+  bool CheckError(unsigned int program, unsigned int flag, bool isProgram, const std::string &errorMessage);
+  int GetUniformLocation(const std::string& name);
 };
 
 #endif // !SHADER_H
