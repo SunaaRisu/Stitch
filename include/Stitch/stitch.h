f49dd39cb869b0d7147cf625595de45d1b94e528
Sunaa Risu <sunaa@sunaarisu.de>
2025-04-18 21:46:41 +0200

    added new stuff to include file

--- include/Stitch/stitch.h
+++ include/Stitch/stitch.h
@@ -6,5 +6,7 @@
 
 #include "../../Stitch/src/Renderer/shader.h"
 #include "../../Stitch/src/Renderer/vao.h"
+#include "../../Stitch/src/Renderer/ib.h"
+#include "../../Stitch/src/Renderer/renderer.h"
 
 // ------------------------------
